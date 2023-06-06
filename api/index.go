8b48ddafa4e03fec6ff8c0cd89228df18ea4c7ac
fubang <derek@derekdeMacBook-Pro.local>
2023-06-06 23:47:30 +0800

    Remove the redirect home page

--- api/index.go
+++ api/index.go
@@ -3,13 +3,15 @@ package api
 import (
 	"adams549659584/go-proxy-bingai/api/helper"
 	"adams549659584/go-proxy-bingai/common"
+	"fmt"
 	"net/http"
 	"strings"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/" {
-		http.Redirect(w, r, common.PROXY_WEB_PAGE_PATH, http.StatusFound)
+		// http.Redirect(w, r, common.PROXY_WEB_PAGE_PATH, http.StatusFound)
+		fmt.Fprintf(w, "Hello, World!")
 		return
 	}
 	if strings.HasPrefix(r.URL.Path, "/turing") {
